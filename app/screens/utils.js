83e974c428367fe2caa09a8f89eaf2e47ab1ebed
omegaspard <gdiopdub@gmail.com>
2021-04-23 19:58:06 +0200

    Create empty method to wait for font.

--- app/screens/utils.js
+++ app/screens/utils.js
@@ -1,4 +1,6 @@
 import { Dimensions, StyleSheet } from 'react-native';
+import { Asset, Font ] from 'expo';
+
 
 const apiKey = '152c208e0cd90790c96881d352e78c49';
 	
@@ -6,6 +8,10 @@ export const fetchWeather = (city) => {
 	var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey+'&units=metric';
 		
 	return fetch(apiURL).then((response) => response.json())
+}
+
+export function waitLoadingFon() {
+
 }
 
 export const style = StyleSheet.create({

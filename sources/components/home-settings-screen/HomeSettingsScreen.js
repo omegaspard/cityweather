10adb14f690ea9f38f4c2be493c9c7c9f170d71e
omegaspard <gdiopdub@gmail.com>
2021-05-05 23:25:48 +0200

    Start adding numeric input.

--- sources/components/home-settings-screen/HomeSettingsScreen.js
+++ sources/components/home-settings-screen/HomeSettingsScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { View, Text, Button } from 'react-native';
+import { View, Text, Buttonn, FlatList, ScrollView } from 'react-native';
+import NumericInput from 'react-native-numeric-input';
+
+const style = require('@app/components/common/styles').style;
+
 
 export default class HomeSettingsScreen extends React.Component {
 	constructor(props) {
@@ -8,18 +12,27 @@ export default class HomeSettingsScreen extends React.Component {
 		this.navigation = props.navigation;
 
 		this.state = {
-
+			homeCitiesNumber: 10,
 		}
 	}
 
 	render = () => {
 		return (
-			<View>
-				<Text>
-					Settings Screen
-					<Button onPress={() => this.navigation.goBack()} title="Go Back Home" />
-				</Text>
-			</View>
+			<ScrollView>
+				<View style={style.container}>
+					<View style={{ flexDirection: 'row', backgroundColor: 'black', width: '100%', fontFamily: 'DMSans-Regular' }}>
+						<Text style={style.titleContainer}>Settings</Text>
+					</View>
+				</View>
+				<NumericInput
+					value={this.state.homeCitiesNumber} 
+					onChange={value => this.setState({homeCitiesNumber: value})}
+					minValue={0}
+					maxValue={25}
+					initValue={this.state.homeCitiesNumber}
+					/> 
+				<NumericInput onChange={value => console.log(value)} />
+			</ScrollView>
 		)
 	}
 }
